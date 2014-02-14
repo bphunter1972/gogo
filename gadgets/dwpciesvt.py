52988656d578f0956bbd8939748b37f2ea9b3402
Brian Hunter <brian.p.hunter@gmail.com>
2014-02-14 14:47:31 -0800

    Get dwpciesvt using check_dependencies.

--- gadgets/dwpciesvt.py
+++ gadgets/dwpciesvt.py
@@ -9,31 +9,29 @@ class DWPciesvtGadget(gadget.Gadget):
         super(DWPciesvtGadget, self).__init__()
       
         self.schedule_phase = 'pre_vlog'
-
         self.name = 'DWPciesvt'
         self.queue = 'build'
         self.interactive = True
         self.runmod_modules = ["synopsys-designware"]
 
-    #--------------------------------------------     
-    def create_cmds(self):
-        "Prepend the commands with a call to dw_vip_setup, if necessary."
-
         # make the obj/unit directory if it doesn't already exist
-        self.dw_dir = os.path.join(gvars.PROJ.VKITS_DIR, "designware")
-        self.unit_dir = os.path.join(self.dw_dir, "obj/unit")
+        self.cwd = os.path.abspath(os.path.join(gvars.PROJ.VKITS_DIR, "designware"))
+        self.unit_dir = os.path.join(self.cwd, "obj/unit")
         try:
             os.makedirs(self.unit_dir)
         except OSError:
             pass
 
-        self.create_dw = os.path.exists('%s/include/sverilog/svt_pcie.uvm.pkg' % self.unit_dir) == False
-        self.cwd = os.path.abspath(self.dw_dir)
+    #--------------------------------------------     
+    def create_cmds(self):
+        "Prepend the commands with a call to dw_vip_setup, if necessary."
 
-        # if we must, prepend this command
-        if self.create_dw:
-            gvars.Log.info("Running dw_vip_setup in %s" % self.cwd)
-            return ["dw_vip_setup -p obj/unit -a pcie_device_agent_svt -svlog"]
-        else:
-            return []
+        return [("Running dw_vip_setup in %s" % self.cwd, "dw_vip_setup -p obj/unit -a pcie_device_agent_svt -svlog")]
+
+    #--------------------------------------------
+    def check_dependencies(self):
+        "Return True if the svt_pcie.uvm.pkg does not exist."
+
+        svt_pcie_uvm_pkg = os.path.join(self.unit_dir, 'include/sverilog/svt_pcie.uvm.pkg')
+        return not os.path.exists(svt_pcie_uvm_pkg)
 
